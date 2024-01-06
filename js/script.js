7601c99587e08a2451306ea0703bc3ef5d9b355d
Jose <josecarlos099916@gmail.com>
2024-01-05 21:21:51 -0300

    Testando um outro código

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ function toggleMenu() {
     if (event.type === 'touchstart') {
         event.preventDefault()
     }
+
     const nav = document.getElementById('nav') // SE REFERE A TAG NAV
     nav.classList.toggle('active') // ADICIONA A CLASSE ACTIVE À NAV
     
@@ -20,3 +21,11 @@ function toggleMenu() {
 // OQ FAZ O EVENTO 'CLICK' NO MOBILE? ELE EMULA UM EVENTO DE TOUCH PORÉM LEVA 300MS. PARA RESOLVER ISSO É POSSÍVEL ADICIONAR O EVENTO 'TOUCHSTART'. MAS QUANDO FAZEMOS ISSO CRIAMOS UM PROBLEMA: ASSIM QUE O USUÁRIO CLICA NO BOTÃO PELO MOBILE ATIVA O EVENTO TOUCHSTART E LOGO EM SEGUIDA ATIVA O CLICK, E ACABA FECHANDO O MENU. PARA ISSO CRIAMOS UMA CONDIÇÃO DENTRO DA FUNÇÃO
 btnMobile.addEventListener('click', toggleMenu)
 btnMobile.addEventListener('touchstart', toggleMenu)
+
+/////////////////////////////////////////////////////// TESTE
+
+Input.addEventListener(“touchstart”, function(event){ 
+    event.preventDefault()
+    //decide o que a funcao vai fazer
+    
+})
