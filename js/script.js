e6c7997bcad3448b011037f570f5191541a1d723
Jose <josecarlos099916@gmail.com>
2024-01-07 22:11:39 -0300

    Consertando a função ('touchstart')

--- js/script.js
+++ js/script.js
@@ -1,9 +1,16 @@
 const btnMobile = document.getElementById('btn-mobile')
 
 function toggleMenu() {
+    btnMobile.addEventListener('touchstart', function(event){ 
+        event.preventDefault()
+        //decide o que a funcao vai fazer
+    })
+
+    /*
     if (event.type === 'touchstart') {
         event.preventDefault()
     }
+    */
 
     const nav = document.getElementById('nav') // SE REFERE A TAG NAV
     nav.classList.toggle('active') // ADICIONA A CLASSE ACTIVE À NAV
@@ -19,13 +26,15 @@ function toggleMenu() {
 }
 
 // OQ FAZ O EVENTO 'CLICK' NO MOBILE? ELE EMULA UM EVENTO DE TOUCH PORÉM LEVA 300MS. PARA RESOLVER ISSO É POSSÍVEL ADICIONAR O EVENTO 'TOUCHSTART'. MAS QUANDO FAZEMOS ISSO CRIAMOS UM PROBLEMA: ASSIM QUE O USUÁRIO CLICA NO BOTÃO PELO MOBILE ATIVA O EVENTO TOUCHSTART E LOGO EM SEGUIDA ATIVA O CLICK, E ACABA FECHANDO O MENU. PARA ISSO CRIAMOS UMA CONDIÇÃO DENTRO DA FUNÇÃO
+
 btnMobile.addEventListener('click', toggleMenu)
 btnMobile.addEventListener('touchstart', toggleMenu)
 
 /////////////////////////////////////////////////////// TESTE
 
-Input.addEventListener(“touchstart”, function(event){ 
+/*
+btnMobile.addEventListener('touchstart', function(event){ 
     event.preventDefault()
     //decide o que a funcao vai fazer
-    
 })
+*/
